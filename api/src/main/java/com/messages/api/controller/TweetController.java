3afb6f194e910a82911f879090ad8ce124a08596
Brunna Serafina <brunnaserafina@gmail.com>
2023-06-28 18:30:55 -0300

    feat: added GET /tweets endpoint with pagination

--- api/src/main/java/com/messages/api/controller/TweetController.java
+++ api/src/main/java/com/messages/api/controller/TweetController.java
@@ -1,8 +1,12 @@
 package com.messages.api.controller;
 
 import com.messages.api.dto.TweetDTO;
+import com.messages.api.model.Tweet;
 import com.messages.api.service.TweetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,4 +24,8 @@ public class TweetController {
         service.save(req);
     }
 
+    @GetMapping
+    public Page<Tweet> getTweets(@PageableDefault(page = 0, size = 5) Pageable page){
+        return service.findAll(page);
+    }
 }
